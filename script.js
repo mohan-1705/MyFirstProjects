ba7921a36a4761ac21226d8c0922f2a4970f1753
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2024-07-19 08:58:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function appendNumber(number) {
+    if (display.textContent === '0') {
+        display.textContent = number;
+    } else {
+        display.textContent += number;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
